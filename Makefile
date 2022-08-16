5ef8a9d4d425e568319bf415142295ab9f2ad9b0
volkov.p <lambdahhh@gmail.com>
2022-08-16 13:48:50 +0300

    add container DI

--- Makefile
+++ Makefile
@@ -1,11 +1,11 @@
-init: down pull build up
+init: down pull build up api-init
 restart: down up
 
-up:
-  docker compose up -d --remove-orphans
-down:
-  docker compose down
-pull:
-  docker compose pull
-build:
-  docker compose build
+up: docker compose up -d --remove-orphans
+down: docker compose down
+pull: docker compose pull
+build: docker compose build
+
+api-init: api-composer-install
+
+api-composer-install: docker composer run --rm api-php-cli composer install
--- api/public/index.php
+++ api/public/index.php
@@ -1,15 +1,30 @@
 <?php
+
+use DI\Container;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
+http_response_code(500);
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$app = AppFactory::create();
+$builder = new DI\ContainerBuilder();
+$builder->addDefinitions([
+    'config' => [
+        'debug' => (bool) getenv('APP_DEBUG')
+    ]
+]);
+
+$container = $builder->build();
+
+$app = AppFactory::create(null, $container);
+
+$app->addErrorMiddleware($container->get('config')['debug'], true, true);
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("{}");
-    return $response;
+    return $response->withHeader('Content-Type', 'application/json');
 });
 
 $app->run();
