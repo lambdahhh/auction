f890639f234c34796f50d743ba46b0007dd6d906
volkov.p <lambdahhh@gmail.com>
2022-08-24 17:33:21 +0300

    not found functional test

--- api/tests/Functional/HomeTest.php
+++ api/tests/Functional/HomeTest.php
@@ -11,4 +11,18 @@ class HomeTest extends WebTestCase
         self::assertEquals(200, $response->getStatusCode());
         self::assertEquals('[]', $response->getBody()->getContents());
     }
+
+    public function testMethod(): void
+    {
+        $response = $this->app()->handle(self::json('POST', '/'));
+
+        self::assertEquals(405, $response->getStatusCode());
+    }
+
+    public function testNotFound(): void
+    {
+        $response = $this->app()->handle(self::json('GET', '/not-found'));
+
+        self::assertEquals(404, $response->getStatusCode());
+    }
 }
